8cf36d5260b755412dd28bc43ad3c704710a5433
Andrii Pivovarov <Andro999b@gmail.com>
2020-01-08 22:05:48 +0200

    fix videocdn

--- functions/providers/VideoCDNProvider.js
+++ functions/providers/VideoCDNProvider.js
@@ -10,11 +10,13 @@ class VideoCDNProvider extends Provider {
     async search(query) {
         const { baseUrl, token, types, timeout } = this.config
 
-        const promices = types.map((type) => 
-            superagent
-                .get(`${baseUrl}/${type}?direction=desc&field=global&limit=10&ordering=last_episode_accepted&page=1&query=${encodeURIComponent(query)}&api_token=${token}`)
+        const promices = types.map((type) => {
+            const ordering = type.endsWith('series') ? 'last_episode_accepted' : 'last_media_accepted'
+
+            return superagent
+                .get(`${baseUrl}/${type}?direction=desc&field=global&limit=10&ordering=${ordering}&page=1&query=${encodeURIComponent(query)}&api_token=${token}`)
                 .timeout(timeout)
-        )
+        })
 
         const results = await Promise.all(promices)
         
@@ -22,13 +24,14 @@ class VideoCDNProvider extends Provider {
             .map((res) => JSON.parse(res.text).data)
             .reduce((acc, item) => acc.concat(item), [])
             .map(({ id, ru_title}) => ({
+                provider: this.name, 
                 id,
                 name: ru_title
             }))
     }
 
     async getInfo(id) {
-        const { baseUrl, token, types, timeout } = this.config
+        const { baseUrl, token } = this.config
 
         const res = await superagent.get(`${baseUrl}/short?api_token=${token}&id=${id}`)
 
@@ -39,8 +42,7 @@ class VideoCDNProvider extends Provider {
         const url = iframe_src.startsWith('//') ? 'https:' + iframe_src: iframe_src
 
         const files = await videocdnembed(url)
-        
-
+    
         return {
             title,
             files,
