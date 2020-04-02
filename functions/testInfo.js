181615ff4ca1808284dee793d798690ee20ed040
Andrii Pivovarov <Andro999b@gmail.com>
2020-04-02 18:03:19 +0300

    fix 7 serialov

--- functions/testInfo.js
+++ functions/testInfo.js
@@ -1,8 +1,8 @@
 const providers = require('./providers')
 const util = require('util')
 
-const provider = 'kinogo'
-const id = 'https://kinogo.by/258-piraty-karibskogo-morya-na-krayu-sveta_2007-27-12.html'
+const provider = '7serealov'
+const id = 'http://7serialov.net/load/komedija/druzja_46/10-1-0-51'
 
 providers.getInfo(provider, id)
     .then((details) => console.log('details',  util.inspect(details, false, null, true)))// eslint-disable-line
