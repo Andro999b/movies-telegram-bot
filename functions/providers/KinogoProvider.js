3bf6b10cfdc225be24d11979b1da17fdc244b23e
Andrii Pivovarov <Andro999b@gmail.com>
2020-10-18 12:23:03 +0300

    fix kinogo

--- functions/providers/KinogoProvider.js
+++ functions/providers/KinogoProvider.js
@@ -3,6 +3,11 @@ const superagent = require('superagent')
 const urlencode = require('urlencode')
 const videocdnembed = require('../utils/videocdnembed')
 
+const { extractString } = require('../utils/extractScriptVariable')
+const parsePlayerJSFile = require('../utils/parsePlayerJSFile')
+const convertPlayerJSPlaylist = require('../utils/convertPlayerJSPlaylist')
+const stripPlayerJSConfig = require('../utils/stripPlayerJSConfig')
+
 class KinogoProvider extends DataLifeProvider {
     constructor() {
         super('kinogo', {
@@ -27,12 +32,13 @@ class KinogoProvider extends DataLifeProvider {
                 },
                 files: {
                     selector: 'ul.tabs',
-                    transform: async ($el) => {
+                    transform: async ($el, $root) => {
                         const onlcickAttr = $el.children().eq(1).attr('onclick')
 
                         let matches = onlcickAttr.match(/\/engine\/ajax\/player_vse_pc\.php\?string_name=\d+/)
 
-                        if (matches.length == 0) return []
+                        if (!matches || matches.length == 0) 
+                            return this._extractFallback($root.find('#1212'))
 
                         const iframeRes = await superagent
                             .get(`${this.config.baseUrl}${matches[0]}`)
@@ -53,6 +59,66 @@ class KinogoProvider extends DataLifeProvider {
                 }
             }
         })
+    }
+
+    _extractFallback($el) {
+        const script = $el.nextAll(':not([src])').toArray()[0].children[0].data
+
+        var files = this._tryExtractMp4(script)
+
+        if (!files) {
+            files = this._tryExtractHls(script)
+        }
+
+        if (!files) {
+            files = this._tryExtractFiles(script)
+        }
+
+        files = files || []
+
+        return files.map((item, index) => ({
+            id: index,
+            ...item
+        }))
+    }
+
+    _tryExtractHls(script) {
+        const fhls = extractString(script, 'fhls')
+
+        if (fhls) {
+            const manifestUrl = this._extractManifest(fhls)
+
+            return [{
+                manifestUrl
+            }]
+        }
+    }
+
+    _tryExtractMp4(script) {
+        const fmp4 = extractString(script, 'fmp4')
+
+        if (fmp4) {
+            const urls = parsePlayerJSFile(fmp4)
+            
+            const url = urls[0].url
+
+            if (url.endsWith('m3u8')) { // not actual mp4 lol
+                return [{
+                    manifestUrl: url
+                }]
+            } else {
+                return [{ urls }]
+            }
+        }
+    }
+
+    _tryExtractFiles(script) {
+        const config = stripPlayerJSConfig(script)
+
+        if (config) {
+            const { file } = config
+            return convertPlayerJSPlaylist(file)
+        }
     }
 
     _getSiteEncoding() {
--- functions/test/testInfo.js
+++ functions/test/testInfo.js
@@ -2,7 +2,7 @@ const providers = require('../providers')
 const util = require('util')
 
 const provider = 'kinogo'
-const id = 'https://kinogo.by/20434-pacany-1-2-sezon.html'
+const id = 'https://kinogo.by/5611-watch-online-movie-forma-golosa_koe-no-katachi_2016.html'
 
 providers.getInfo(provider, id)
     .then((details) => console.log(util.inspect(details, false, null, true)))// eslint-disable-line
