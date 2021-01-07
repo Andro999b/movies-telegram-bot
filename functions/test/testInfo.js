22dd4062c950d18f6414241389363f5ac363ac0b
Andrii Pivovarov <Andro999b@gmail.com>
2021-01-07 02:02:56 +0200

    fix anigit

--- functions/test/testInfo.js
+++ functions/test/testInfo.js
@@ -1,10 +1,10 @@
 const providers = require('../providers')
 const util = require('util')
 
-const provider = 'kinogo'
-const id = 'https%3A%2F%2Fkinogo.by%2F14452-fitnes-1-2-3-4-sezon.html'
+const provider = 'nekomori'
+const id = '150'
 
-console.log(decodeURIComponent(id));
+console.log(decodeURIComponent(id))
 
 providers.getInfo(provider, id)
     .then((details) => console.log(util.inspect(details, false, null, true)))// eslint-disable-line
