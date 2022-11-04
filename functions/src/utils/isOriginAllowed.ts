152426276f1a2f0b21794486e74c45212893e63f
Andrii Pyvovarov <andro999b@gmail.com>
2022-11-04 18:36:38 +0200

    setup typescript for be

---
+++ functions/src/utils/isOriginAllowed.ts
@@ -0,0 +1,11 @@
+import { APIGatewayProxyEvent } from 'aws-lambda'
+
+const ALLOWED_DOMAINS = process.env.ALLOWED_DOMAINS!.split(',')
+
+export default (event: APIGatewayProxyEvent): boolean => {
+  const origin = event.headers?.origin
+  if (!origin)
+    return false
+
+  return ALLOWED_DOMAINS.findIndex((domain) => origin.endsWith(domain)) != -1
+}
