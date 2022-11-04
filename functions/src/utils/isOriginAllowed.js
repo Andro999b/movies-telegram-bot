cf855c304f3eb1d1529348612b17de3b3c2b999a
Andrii Pyvovarov <andro999b@gmail.com>
2022-11-04 18:36:38 +0200

    setup typescript for be

--- functions/src/utils/isOriginAllowed.js
+++
@@ -1,4 +0,0 @@
-const ALLOWED_DOMAINS = process.env.ALLOWED_DOMAINS.split(',')
-
-module.exports = (event) =>
-    event.headers?.origin && ALLOWED_DOMAINS.findIndex((domain) => event.headers.origin.endsWith(domain)) != -1
