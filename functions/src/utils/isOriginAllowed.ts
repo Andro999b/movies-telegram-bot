69c847422bb57c7a73e403f4e1c986d5d3407404
Andrii Pyvovarov <Andro999b@gmail.com>
2023-01-11 16:14:30 +0000

    Disable anidub

--- functions/src/utils/isOriginAllowed.ts
+++ functions/src/utils/isOriginAllowed.ts
@@ -3,6 +3,9 @@ import { APIGatewayProxyEvent } from 'aws-lambda'
 const ALLOWED_DOMAINS = process.env.ALLOWED_DOMAINS.split(',')
 
 export default (event: APIGatewayProxyEvent): boolean => {
+  if(ALLOWED_DOMAINS.length == 0)
+    return true
+
   const origin = event.headers?.origin ?? event.headers?.Origin
   if (!origin)
     return false
